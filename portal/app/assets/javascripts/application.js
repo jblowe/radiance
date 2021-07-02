e2667319b0b89a145b97fddd30df9fdae3d230e3
John Lowe <jblowe@berkeley.edu>
2021-07-02 10:45:51 -0700

    HMP-178: update application(.css,.js) for blacklight v7

--- portal/app/assets/javascripts/application.js
+++ portal/app/assets/javascripts/application.js
@@ -1,4 +1,7 @@
 //= require jquery
+//= require 'blacklight_advanced_search'
+
+
 //= require jquery3
 //= require rails-ujs
 //= require turbolinks
@@ -9,4 +12,10 @@
 //= require twitter/typeahead
 //= require bootstrap
 //= require blacklight/blacklight
+
+
+
+// For blacklight_range_limit built-in JS, if you don't want it you don't need
+// this:
+//= require 'blacklight_range_limit'
 
